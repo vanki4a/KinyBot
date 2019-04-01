b9338e749ab9d9d6d3c6e0d0abe49cf9be441fe3
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-04-02 01:29:21 +0300

    Update kinybot.py

--- kinybot.py
+++ kinybot.py
@@ -1,7 +1,15 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
 import asyncio
+import platform
+import colorsys
+import random
+import os
 import time
+from discord.voice_client import VoiceClient
+from discord import Game, Embed, Color, Status, ChannelType
 
 bot = commands.Bot(command_prefix=';')
 ivan='445198123837554688'
@@ -19,12 +27,12 @@ async def ping():
 	await bot.say(':ping_pong:')
 	await bot.say('**You pinged me haha**')
 	
-@bot.command(pass_context=True)
-async def warn(ctx,target:discord.Member): 
-    if ctx.message.author.id==(ivan):
-      await bot.send_message(target,'**Warning!**')
-    else:
-     	await bot.say('**No permissions!**')
+@bot.command(pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def warn(ctx, userName: discord.User, *, message:str): 
+    await bot.send_message(userName, "You have been warned for: **{}**".format(message))
+    await bot.say(":warning: __**{0} Has Been Warned!**__ :warning: ** Reason:{1}** ".format(userName,message))
+    pass
 	
 @bot.command()
 async def invite():
@@ -45,8 +53,7 @@ async def porn():
 async def support():
 	await bot.say('https://discord.gg/sxNAB3e')
 	await bot.say('**Support group for bugs in bot!**')
-	
-	
+		
 @bot.command()
 async def twitch():
 	await bot.say(':mouse_three_button:')
@@ -79,7 +86,7 @@ async def hack():
 @bot.command()
 async def new():
 	await bot.say(':confetti_ball:')
-	await bot.say('``1.04.2019`` | **New prefix ";" | Enjoy**')
+	await bot.say('``1.04.2019`` | **New prefix ";" | New command-userinfo,membercount,embed,serverinfo,clear |  Enjoy**')
 	
 @bot.command(pass_context=True)
 async def uptime(ctx):
@@ -104,5 +111,152 @@ async def website():
 async def partnership():
 	await bot.say(':trophy:')
 	await bot.say('``To be a partner with us, you must have a group of at least 50-100 people in it``')
-  
-  client.run(os.getenv('Token'))
+	
+@bot.command(pass_context = True)
+@commands.has_permissions(kick_members=True)     
+async def userinfo(ctx, user: discord.Member):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color = discord.Color((r << 16) + (g << 8) + b))
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="`ID``", value=user.id, inline=True)
+    embed.add_field(name="``Status``", value=user.status, inline=True)
+    embed.add_field(name="``Highest role``", value=user.top_role)
+    embed.add_field(name="``Joined``", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+    
+@bot.command(pass_context=True, aliases=['server'])
+@commands.has_permissions(kick_members=True)
+async def membercount(ctx, *args):
+    """
+    Shows stats and information about current guild.
+    ATTENTION: Please only use this on your own guilds or with explicit
+    permissions of the guilds administrators!
+    """
+    if ctx.message.channel.is_private:
+        await bot.delete_message(ctx.message)
+        return
+
+    g = ctx.message.server
+
+    gid = g.id
+    membs = str(len(g.members))
+    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
+    users = str(len([m for m in g.members if not m.bot]))
+    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
+    bots = str(len([m for m in g.members if m.bot]))
+    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
+    created = str(g.created_at)
+    
+    em = Embed(title="Membercount")
+    em.description =    "```\n" \
+                        "Members:   %s (%s)\n" \
+                        "  Users:   %s (%s)\n" \
+                        "  Bots:    %s (%s)\n" \
+                        "Created:   %s\n" \
+                        "```" % (membs, membs_on, users, users_on, bots, bots_on, created)
+
+    await bot.send_message(ctx.message.channel, embed=em)
+    await bot.delete_message(ctx.message)
+    
+@bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def embed(ctx, *args):
+    """
+    Sending embeded messages with color (and maby later title, footer and fields)
+    """
+    argstr = " ".join(args)
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    text = argstr
+    color = discord.Color((r << 16) + (g << 8) + b)
+    await bot.send_message(ctx.message.channel, embed=Embed(color = color, description=text))
+    
+@bot.command(pass_context=True)  
+@commands.has_permissions(kick_members=True)     
+
+async def serverinfo(ctx):
+    '''Displays Info About The Server!'''
+
+    server = ctx.message.server
+    roles = [x.name for x in server.role_hierarchy]
+    role_length = len(roles)
+
+    if role_length > 50: #Just in case there are too many roles...
+        roles = roles[:50]
+        roles.append('>>>> Displaying[50/%s] Roles'%len(roles))
+
+    roles = ', '.join(roles);
+    channelz = len(server.channels);
+    time = str(server.created_at); time = time.split(' '); time= time[0];
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    join = discord.Embed(description= '%s '%(str(server)),title = 'Server Name', color = discord.Color((r << 16) + (g << 8) + b));
+    join.set_thumbnail(url = server.icon_url);
+    join.add_field(name = '__Owner__', value = str(server.owner) + '\n' + server.owner.id);
+    join.add_field(name = '__ID__', value = str(server.id))
+    join.add_field(name = '__Member Count__', value = str(server.member_count));
+    join.add_field(name = '__Text/Voice Channels__', value = str(channelz));
+    join.add_field(name = '__Roles (%s)__'%str(role_length), value = roles);
+    join.set_footer(text ='Created: %s'%time);
+
+    return await bot.say(embed = join);
+    
+@bot.command(pass_context = True)
+@commands.has_permissions(manage_messages=True)  
+async def clear(ctx, number):
+ 
+    if ctx.message.author.server_permissions.manage_messages:
+         mgs = [] #Empty list to put all the messages in the log
+         number = int(number) #Converting the amount of messages to delete to an integer
+    async for x in bot.logs_from(ctx.message.channel, limit = number+1):
+        mgs.append(x)            
+       
+    try:
+        await bot.delete_messages(mgs)          
+        await bot.say(str(number)+' messages deleted')
+     
+    except discord.Forbidden:
+        await bot.say(embed=Forbidden)
+        return
+    except discord.HTTPException:
+        await bot.say('clear failed.')
+        return         
+   
+ 
+    await bot.delete_messages(mgs)
+    
+@bot.command(pass_context=True)
+async def poll(ctx, question, *options: str):
+        if len(options) <= 1:
+            await bot.say('``You need more than one option to make a poll!``')
+            return
+        if len(options) > 10:
+            await bot.say('``You cannot make a poll for more than 10 things!``')
+            return
+
+        if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+            reactions = ['👍', '👎']
+        else:
+            reactions = ['1\u20e3', '2\u20e3', '3\u20e3', '4\u20e3', '5\u20e3', '6\u20e3', '7\u20e3', '8\u20e3', '9\u20e3', '\U0001f51f']
+
+        description = []
+        for x, option in enumerate(options):
+            description += '\n {} {}'.format(reactions[x], option)
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+        embed = discord.Embed(title=question, description=''.join(description), color = discord.Color((r << 16) + (g << 8) + b))
+        react_message = await bot.say(embed=embed)
+        for reaction in reactions[:len(options)]:
+            await bot.add_reaction(react_message, reaction)
+        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
+        await bot.edit_message(react_message, embed=embed)
+
+@bot.command(pass_context=True)
+async def guess(ctx, number):
+    try:
+        arg = random.randint(1, 10)
+    except ValueError:
+        await bot.say("``Invalid number``")
+    else:
+        await bot.say('The correct answer is ' + str(arg))
+	
+
+ client.run(os.getenv('Token'))
