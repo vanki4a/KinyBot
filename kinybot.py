ec5e8b3ef68c5212235f8d3ffce6fe10c7eb0679
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-05-06 16:21:59 +0300

    Update kinybot.py

--- kinybot.py
+++ kinybot.py
@@ -16,6 +16,11 @@ import re
 import json
 import traceback
 
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
+client = Bot(description="KinyBot", command_prefix=";", pm_help = True)
+client.remove_command('help')
+
+
 client = commands.Bot(command_prefix=';')
 ivan='445198123837554688'
 start=time.time()
